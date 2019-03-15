276bcca8b88530929b56b42eb19fdc4b4c3279e5
Maekee <drmicke@gmail.com>
2019-03-15 12:57:32 +0100

    Update RunAsSYSTEM.ps1

--- RunAsSYSTEM.ps1
+++ RunAsSYSTEM.ps1
@@ -4,4 +4,4 @@
 Start-Process -FilePath cmd.exe -Verb Runas -ArgumentList '/k C:\SysinternalsSuite\PsExec.exe -i -s powershell.exe'
 
 #You can also run as a managed account (gMSA) with:
-Start-Process -FilePath cmd.exe -Verb Runas -ArgumentList '/k C:\SysinternalsSuite\PsExec.exe -i -u aklagare\gmsa-accountnamehere$ powershell.exe'
+Start-Process -FilePath cmd.exe -Verb Runas -ArgumentList '/k C:\SysinternalsSuite\PsExec.exe -i -u domain\gmsa-accountnamehere$ powershell.exe'
